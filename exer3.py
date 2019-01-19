052018505f800cc90cee693c17972129d587022d
Adilmar Dantas <adilmarcoelho@hotmail.com>
2019-01-18 23:13:21 -0200

    upade codes

--- exer3.py
+++ exer3.py
@@ -1,9 +1,9 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('images/video.avi',fourcc, 20.0, (640,480))
+out = cv2.VideoWriter('videos/video1.avi',fourcc, 20.0, (640,480))
 
 while(True):
     ret, frame = cap.read()
