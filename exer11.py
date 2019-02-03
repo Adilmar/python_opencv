0942b20c54e6525e596da5029fdd0b53da86a7a4
Adilmar Dantas <adilmarcoelho@hotmail.com>
2019-02-03 13:06:53 -0200

    update course

--- exer11.py
+++ exer11.py
@@ -7,10 +7,11 @@ while(1):
     _, frame = cap.read()
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     
+    lower_blue = np.array([60,70,60])
     lower_red = np.array([30,150,50])
     upper_red = np.array([255,255,180])
     
-    mask = cv2.inRange(hsv, lower_red, upper_red)
+    mask = cv2.inRange(hsv, lower_blue, upper_red)
     res = cv2.bitwise_and(frame,frame, mask= mask)
 
     cv2.imshow('frame',frame)
