991a09fadbd3be69083a68b5e181b2efb2584de3
Adilmar Dantas <akanehar@gmail.com>
2019-02-08 19:35:10 -0200

    update codes

--- codes/exer9.py
+++ codes/exer9.py
@@ -1,12 +1,26 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('images/s1.jpg')
-grayscaled = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-retval, threshold = cv2.threshold(grayscaled, 10, 255, cv2.THRESH_BINARY)
-cv2.imshow('original',img)
-cv2.imshow('threshold',threshold)
-cv2.waitKey(0)
+cap = cv2.VideoCapture(1)
+
+while(1):
+
+    _, frame = cap.read()
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    
+    lower_red = np.array([30,150,50])
+    upper_red = np.array([255,255,180])
+    
+    mask = cv2.inRange(hsv, lower_red, upper_red)
+    res = cv2.bitwise_and(frame,frame, mask= mask)
+
+    cv2.imshow('Original',frame)
+    edges = cv2.Canny(frame,100,200)
+    cv2.imshow('Edges',edges)
+
+    k = cv2.waitKey(5) & 0xFF
+    if k == 27:
+        break
+
 cv2.destroyAllWindows()
-
-
+cap.release()
